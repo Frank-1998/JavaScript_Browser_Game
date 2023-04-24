8f54a638f43f830c79e606553276fbeaec900063
Frank-1998 <ziang6@ualberta.ca>
2023-04-24 14:55:07 -0600

    add image to the obstacle position

--- script.js
+++ script.js
@@ -65,9 +65,17 @@ window.addEventListener('load', function(){
             this.collisionX = Math.random() * this.game.width;
             this.collisionY = Math.random() * this.game.height;
             this.collisionRadius = 60;
+            this.image = document.getElementById('obstacles');
+            this.spriteWidth = 250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 70;
         }
 
         draw(context){
+            context.drawImage(this.image, 0, 0, this.width, this.height, this.spriteX, this.spriteY, this.spriteWidth, this.spriteHeight);
             context.beginPath();
             context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
             context.save(); // to limit the canvase setting use save and restore.
@@ -85,7 +93,7 @@ window.addEventListener('load', function(){
             this.height = canvas.height;
             this.player = new Player(this);
             this.obstacles = [];
-            this.numberOfObstacles = 5;
+            this.numberOfObstacles = 1;
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
