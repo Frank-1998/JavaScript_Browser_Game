5beb553fb7426b8dfa235822341ddb2eb2786adb
unknown <ziang6@ualberta.ca>
2023-04-27 17:16:14 -0600

    adding collision physics

--- script.js
+++ script.js
@@ -57,8 +57,16 @@ window.addEventListener('load', function(){
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
             // check collision with obstacles
+
             this.game.obstacles.forEach(obstacle => {
-                console.log(this.game.checkCollision(this, obstacle));
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this,obstacle);
+                if (collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
+
+                }
             })
         }
     }
@@ -126,9 +134,9 @@ window.addEventListener('load', function(){
             });
         }
         render(context){
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
             this.player.draw(context);
             this.player.update();
-            this.obstacles.forEach(obstacle => obstacle.draw(context));
         }
 
         checkCollision(a,b){
@@ -136,7 +144,7 @@ window.addEventListener('load', function(){
             const dy = a.collisionY - b.collisionY;
             const distance = Math.hypot(dy, dx);
             const sumOfRadii = a.collisionRadius + b.collisionRadius;
-            return (distance < sumOfRadii);
+            return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy];
         }
 
         init(){ // init all the obstacles and make sure they don't overlap
