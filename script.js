0cb25f7547a76b6a45c0729569d13bde29f2f844
unknown <ziang6@ualberta.ca>
2023-05-21 11:05:04 -0600

    add egg class

--- script.js
+++ script.js
@@ -135,6 +135,36 @@ window.addEventListener('load', function(){
         }
     }
 
+    class Egg {
+        constructor(game){
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 40;
+            this.image = document.getElementById('egg');
+            this.spriteWidth = 110;
+            this.spriteHeight = 135;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX + this.width * 0.5;
+            this.spriteY = this.collisionY + this.height * 0.5;
+
+        }
+
+        draw(context){
+            context.drawImage(this.image, this.spriteX, this.spriteY);
+            if (this.game.debug){
+                context.beginPath();
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+                context.save(); // to limit the canvase setting use save and restore.
+                context.globalAlpha = 0.5;
+                context.fill();
+                context.restore();
+                context.stroke();
+            }
+        }
+    }
+
     class Game {
         constructor(canvas){
             this.canvas = canvas;
@@ -145,7 +175,9 @@ window.addEventListener('load', function(){
             this.timer = 0;
             this.interval = 1000/this.fps;
             this.obstacles = [];
+            this.eggs = [];
             this.numberOfObstacles = 5;
+            this.maxEggs = 10;
             this.topMargine = 260; // the space for the bush in the background
             this.debug = true;
             this.mouse = {
